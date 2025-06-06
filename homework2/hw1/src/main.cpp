85daf4bc434033ae5ca9813a18742dfec6ddb146
xiao070707 <150762823+xiao070707@users.noreply.github.com>
2025-06-06 22:45:45 +0800

    Update main.cpp

--- homework2/hw1/src/main.cpp
+++ homework2/hw1/src/main.cpp
@@ -1,1 +1,27 @@
 
+#include "heap.hpp"
+
+int main() {
+    MinHeap<int> minHeap;
+    MaxHeap<int> maxHeap;
+
+    int num;
+    cout << "請輸入一串整數（以 0 結尾）: ";
+    while (cin >> num && num != 0) {
+        minHeap.Push(num);
+        maxHeap.Push(num);
+    }
+
+    // 印出原始陣列內容
+    cout << "\n======= 陣列輸出 =======\n";
+    minHeap.PrintArray();
+    maxHeap.PrintArray();
+
+    // 印出樹狀結構
+    cout << "\n======= 樹狀結構輸出 =======\n";
+    minHeap.PrintTree();
+    cout << endl;
+    maxHeap.PrintTree();
+
+    return 0;
+}
